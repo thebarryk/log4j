5cc6ab7637e5eeaf26eb0c7fae3efb850833ee11
Barry Krawchuk <thebarryk@gmail.com>
2021-12-27 14:52:40 -0500

    search_log4j_payload.py Refactored defang's, build_query. Option to defang, or not - build_query: returns search query   o uses better pattern for base64 string in the jndi request - defang: shortened the code to match the expressions that need to be   changed to make the output less dangerous   o added a control to defang / or not - matches the one in log4j.ipynb

--- search_log4j_payload.py
+++ search_log4j_payload.py
@@ -1,6 +1,7 @@
 ''' Make a report of payloads delivered with log4j http gets.
     See search_log4j_payload.py
 '''
+
 import base64
 import csv
 import pandas as pd
@@ -13,15 +14,66 @@ import sys
 from time import sleep
 import splunklib.results as results
 import splunklib.client as client
-pp = pprint.PrettyPrinter()
+import re
 
-fn = "log64_attempts.12-13-2012.csv"
+def defang_host(str):
+    return re.sub(r"(https?:\/\/)([^\/]+)", r"\1?\2", str)
+
+def defang_ip(str):
+    return re.sub(r"((?:(?:[0-9]{1,3})(?:\.)?){4})", r"?\1", str)
+
+def defang(str):
+    fang = lambda p, s: re.sub(f"{p}", r"?\1", s)
+    try:
+        if type(str) == str:
+            if len(str) == 0:
+                return str
+        if type(str) == float:
+            if np.isnan(str):
+                return str
+        r = defang_ip( defang_host( fang("(wget|curl|bash|http)", str) ) )       
+    except BaseException as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        import pdb; pdb.set_trace()
+    return r
 
 def quote_words(str):
     '''quote_words("word1 word2") --> "word1",:word2
        Change string of words into string in quoted csv format
     '''
     return ",".join([ f'"{x}"' for x in str.replace("\n","").split()])
+
+def build_query():
+    # Build a query
+    # Reminder: earliest_time = "12/13/2021:00:00:00"
+    
+    earliest_time = "12/14/2021:00:00:00"
+    latest_time ="12/26/2021:00:00:00"
+
+    idx = """directory_services_nonprod doh_google_cloud dol_google_cloud dtf_http estreamer
+    fireeye hcr_google_cloud health_datapower health_network its_google_cloud its_okta
+    nonprod main miauditlogs os otda_google_cloud vmware webny webny_nonprod wineventlog"""
+
+    indexes = f"index IN ({quote_words(idx)})"
+
+    match = '''Base64 Command "${jndi:*}"'''
+
+    rex = '''| rex max_match=5 
+    "({jndi.*?Base64\/(?<b64>(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?))"'''
+
+#     rex = '''| rex "\${jndi.*?Base64\/(?<b64>[^}]*?)}.*"'''
+
+    stats = '''
+    | mvexpand b64 | stats values(host) as host earliest(_time) as earliest  by b64 |  mvexpand host 
+    | convert timeformat="%m/%d/%Y %H:%M:%S" ctime(earliest) as earliest 
+    | sort host +earliest 
+    | table host earliest b64'''
+
+    query = f"""search earliest={earliest_time}
+                latest={latest_time} 
+                {indexes} {match} {rex} {stats}"""
+
+    return query
 
 def d64(x):
     ''' d(b64)-->decodes base64 byte string. =Nan if there's an error
@@ -59,7 +111,6 @@ def list_to_df(lst, columns):
     return pd.DataFrame(sublists, columns=columns)
 
 # This job makes a real search and outputs the result.
-# Assume an authenticated session is open.
 
 HOST = "cnsesplunkoperations.svc.ny.gov"
 PORT = 8089
@@ -77,32 +128,6 @@ service = client.connect(
     owner=OWNER,
     app=APP)
 
-# Build a query
-# Reminder: earliest_time = "12/13/2021:00:00:00"
-earliest_time = "-2d@d"
-latest_time ="-1d@d"
-
-idx = '''directory_services_nonprod doh_google_cloud dol_google_cloud dtf_http estreamer
-fireeye hcr_google_cloud health_datapower health_network its_google_cloud its_okta
-nonprod main miauditlogs os otda_google_cloud vmware webny webny_nonprod wineventlog'''
-
-indexes = f"index IN ({quote_words(idx)})"
-
-match = '''
-Base64 Command "${jndi:*}" | rex "\${jndi.*?Base64\/(?<b64>[^}]*?)}.*"'''
-
-stats = '''
-| mvexpand b64 | stats values(host) as host earliest(_time) as earliest  by b64 |  mvexpand host 
-| convert timeformat="%m/%d/%Y %H:%M:%S" ctime(earliest) as earliest 
-| sort host +earliest 
-| table host earliest b64'''
-
-query = f"""search earliest={earliest_time}
-            latest={latest_time} 
-            {indexes} {match} {stats}"""
-
-# searchquery_normal = "search index=main | stats values(sourcetype)"
-searchquery_normal = query
 # pp.pprint(query)
 
 kwargs_normalsearch = {
@@ -110,6 +135,7 @@ kwargs_normalsearch = {
 #     "latest_time": "-1d@d",
     "search_mode": "normal"
     }
+searchquery_normal = build_query()
 
 job = service.jobs.create(searchquery_normal, **kwargs_normalsearch)
 
@@ -137,18 +163,28 @@ while True:
 # for result in results.ResultsReader(job.results()):
 #     print(result)
 
-save_results = job.results()
+# By default only 100 events are returned. Use count=0 to get all of them to sys limit.
+# See: Splunk Answer 308505
+# https://community.splunk.com/t5/Splunk-Search/Custom-Splunk-search-command-only-returns-100-results/m-p/308505
+    
+# save_results = job.results()
+save_results = job.results(count=0)
 
 lst = make_list(save_results)
 log = list_to_df(lst, columns=["host", "earliest", "base64_payload"])
 
 # Decode the b64 column into new column, payload
-log["payload"] = log.loc[:,"base64_payload"].apply(d64)
-
-# Show the result or save the result 
-# pp.pprint(log[["host", "earliest", "payload"]])
+defanging = False
+if defanging:
+    log["raw_payload"] = log.loc[:,"base64_payload"].apply(d64)
+    # Defang the raw_payload column into new column, payload
+    log["payload"] = log.loc[:,"raw_payload"].apply(defang)
+else:
+    log["payload"] = log.loc[:,"base64_payload"].apply(d64)
 
 log[["host", "earliest", "payload"]].to_csv("search_log4j.txt", index=False)
 
 job.cancel()   
 sys.stdout.write('\n')
+
+
