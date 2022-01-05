3396628874a65b24dca37faf81be2613f54e114d
Barry Krawchuk <thebarryk@gmail.com>
2022-01-05 14:44:33 -0500

    parse_ip_from_payload.py Added time to the output events, dup'd and dedup'd. Added argparse. - time added to each event. Now output is useful for time studies.     o When deduped, only the earliest time is kept for each host     o Change data structure to accumulate hosts and times       to a dict[host] = [t0, t1, t2, ...]     o Did not add any hosts found with fetch_host already found       by fetch_ip avoiding duplicates that make hit count too high - Use argparse to control operation of the program (pretty easy)     o fn ... input file name     o --dedup .. dedup output     o -o OUTPUT .. provide an output file name     o put argparse into a my Myopt class to hide the variables     o Placed in global environment so options can be used anywhere - Code is developed and tested in parse_ip_from_payload.ipynb

--- parse_ip_from_payload.py
+++ parse_ip_from_payload.py
@@ -1,16 +1,34 @@
-''' Read report of web payload's and get list of unique source hosts
-    Usage: parse_ip_from_payload <inputfile> 
+''' Read report of web payload's and get list of source hosts
+    Usage: parse_ip_from_payload FN [--dedup] [-o OUTPUT]
+    --dedup .. include one event per host at the earliest time it occurred
 '''
 import re
 import pandas as pd
 import numpy as np
 import sys
 import ipaddress
+from collections import defaultdict
+import argparse
 
+class Myopt():
+    def __init__(self, args=None):
+
+        self.description = '''Parse ip and hosts from file'''
+        self.p = argparse.ArgumentParser(description=self.description)
+        add = self.p.add_argument
+        add("fn", help="Input file containing ip and hosts")
+        add("--dedup", action="store_true", help="Deduplicate output")
+        add("-o", "--output", help="Output file for time, host", default="log4j_hosts.csv")
+        if args:
+            self.opt = self.p.parse_args(args)
+        else:
+            self.opt = self.p.parse_args()
+            
 def fetch_ip(str):
     ''' fetch_ip(str)-->list of ip adresses in string '''
     ippat = r"((?:(?:[0-9]{1,3})(?:\.)){3}(?:[0-9]{1,3}))"
-    grp = re.findall(ippat, str)  # findall returns empty group
+    grp = re.findall(ippat, str)  
+    # Worst case: findall returns an empty group
     return grp
 
 def fetch_host(cmd):
@@ -22,50 +40,59 @@ def fetch_host(cmd):
 def main():
     ''' Read report of web payload's and get list of unique source hosts '''
 
-    # read the search results from the file supplied as an program argument
+    # read the search results from the file supplied as program argument
 
-    fn = opt.fn
+    fn = myopt.fn
+    # log is dict with columns host, time and payload
     log = pd.read_csv(fn)
 
-    # Clean up
-    # Drop any rows that contain Nan. e.g. some of the web paylod's 
+    # Drop any rows that contain Nan. e.g. some of the web paylod's
     # cannot be converted from base64
     log = log.dropna(subset=["payload"]).reset_index(drop=True)
 
     ## Find the hosts being referenced in the payload's
 
-    hosts = {}
-
-    grp = []
+    events = defaultdict(list)
+    
     for rec, cmd in enumerate(log.payload):
+        
         # Run fetch_ip first to get all the ip addresses
+        # Keep the repetitions because each represents another hit
         r1 = [f"{x}" for x in fetch_ip(cmd)]
-        # Do not aw2chini,d duplicate ip addresses because fetch_ip found them
+        
+        # Do not add any ip addresses which were already found by fetch_ip 
         r2 = [f"{x}" for x in fetch_host(cmd) if x not in r1]
         
-        grp.extend(r1)
-        grp.extend(r2)
+        timestamp = log.time[rec]
+        for host in r1 + r2:
+            events[host].append(timestamp)
 
-    # Deduplicate, make into a df, and output as csv
-    if opt.dedup:
+    # Deduplicate by storing only one time, the earliest of e make into a df, and output as csv
+    if myopt.dedup:
         print("Deduping ..")
-        hosts = set(grp)
+        for host in events:
+            earliest = min(events[host])
+            events[host] = [earliest]
     else:
         print("Not deduping ..")
-        hosts = grp
 
-    # Store to host in column named ip to match name used by whois
-    # whois will not use any that are not valid ip addresses
-    dfhosts = pd.DataFrame(hosts,columns=["ip"])
-    dfhosts.to_csv("log4j_hosts.csv",index=False)
-    return dfhosts
+# Write csv with columns time, ip
+# Store the host in column named ip to match name used by whois
+# Note: whois will not use any that are not valid ip addresses
     
+    with open(myopt.output,"w") as fout:
+        fout.write("time,ip\n")
+        for host, times in events.items():
+            for ztime in times:
+                fout.write(f"{ztime},{host}\n")
 
-import argparse
-parser = argparse.ArgumentParser(description="Parse ip and hosts from file")
-#parser.add_argument(prog="parse_ip_from_payload")
-parser.add_argument("fn", help="Input file containing ip and hosts")
-parser.add_argument("--dedup", action="store_true", help="Deduplicate output")
-opt = parser.parse_args()
+# Establish options in global object, myoptions, so it can be used anywhere.
+
+# myoptions = Myopt("all_jndi_hits_2021.csv -o all_jndi_hosts_2021.csv".split())
+myoptions = Myopt()
+myopt = myoptions.opt  # Makes it easy to get an option e.g. myopt.dedup
 
 main()
+print("Done!")
+
+
